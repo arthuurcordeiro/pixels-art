d07e0e7d624bd99c0078ef1d88e83614aaf733ff
Arthur <acordeiro210691@gmail.com>
2022-11-04 18:39:04 -0300

    cria botão de cores aleatórias

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-
-
 document.querySelectorAll('.color')
+function colorRandom (){
 for (let i = 1; i < document.querySelectorAll('.color').length; i ++){
     let red = Math.abs(Math.random() * 256)
     let green = Math.abs(Math.random() * 256)
     let blue = Math.abs(Math.random() * 256)
     document.querySelectorAll('.color')[0].style.backgroundColor = 'black';
-        document.querySelectorAll('.color')[i].style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    document.querySelectorAll('.color')[i].style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
     }
+}
+document.querySelector('#button-random-color')
+let buttonRandom = document.querySelector('#button-random-color')
+buttonRandom.addEventListener('click', colorRandom)
+colorRandom()
