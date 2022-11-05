888844c4cb41ad1757df8934917d1d7191f20719
Arthur <acordeiro210691@gmail.com>
2022-11-05 01:50:40 -0300

    aplica formatação de código

--- script.js
+++ script.js
@@ -3,125 +3,123 @@ const input = document.querySelector('#board-size');
 const pixelsButton = document.querySelector('#generate-board');
 const vqvButton = document.querySelector('#generate-board');
 
-document.querySelectorAll('.color')
-function colorRandom (){
-    let array = ['black']
-for (let i = 1; i < document.querySelectorAll('.color').length; i ++){
-    let red = Math.abs(Math.random() * 256)
-    let green = Math.abs(Math.random() * 256)
-    let blue = Math.abs(Math.random() * 256)
+document.querySelectorAll('.color');
+function colorRandom() {
+  let array = ['black'];
+  for (let i = 1; i < document.querySelectorAll('.color').length; i++) {
+    let red = Math.abs(Math.random() * 256);
+    let green = Math.abs(Math.random() * 256);
+    let blue = Math.abs(Math.random() * 256);
     document.querySelectorAll('.color')[0].style.backgroundColor = 'black';
-    document.querySelectorAll('.color')[i].style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-    array.push(`rgb(${red}, ${green}, ${blue})`)
-}
-meuStorage(array)
+    document.querySelectorAll('.color')[
+      i
+    ].style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    array.push(`rgb(${red}, ${green}, ${blue})`);
+  }
+  meuStorage(array);
 }
 
-
-document.querySelector('#button-random-color')
-let buttonRandom = document.querySelector('#button-random-color')
-buttonRandom.addEventListener('click', colorRandom)
-const colorPalette = JSON.parse(localStorage.getItem('colorPalette'))
-if (colorPalette != null){
-    let colorStorage = document.querySelectorAll('.color')
-    for (let j = 0; j < colorStorage.length; j++){
-            colorStorage[j].style.backgroundColor = colorPalette[j]
-        } 
-    } else {
-        colorRandom();
-    }
-function meuStorage (array){
-    localStorage.setItem('colorPalette', JSON.stringify(array))
+document.querySelector('#button-random-color');
+let buttonRandom = document.querySelector('#button-random-color');
+buttonRandom.addEventListener('click', colorRandom);
+const colorPalette = JSON.parse(localStorage.getItem('colorPalette'));
+if (colorPalette != null) {
+  let colorStorage = document.querySelectorAll('.color');
+  for (let j = 0; j < colorStorage.length; j++) {
+    colorStorage[j].style.backgroundColor = colorPalette[j];
+  }
+} else {
+  colorRandom();
+}
+function meuStorage(array) {
+  localStorage.setItem('colorPalette', JSON.stringify(array));
 }
 
 window.onload = () => {
-    let savedBoard = localStorage.getItem('boardSize')
-    if (savedBoard != null) {
-        createBoard(savedBoard)
-    } else {
-        createBoard(5)
-    }
-    document.querySelectorAll('.color')[0].className = 'color selected'
-    paintClick();
-    const savedColors = JSON.parse(localStorage.getItem('pixelBoard'))    
-    if (savedColors != null) {
-        applySavedColors();
-    }
+  let savedBoard = localStorage.getItem('boardSize');
+  if (savedBoard != null) {
+    createBoard(savedBoard);
+  } else {
+    createBoard(5);
+  }
+  document.querySelectorAll('.color')[0].className = 'color selected';
+  paintClick();
+  const savedColors = JSON.parse(localStorage.getItem('pixelBoard'));
+  if (savedColors != null) {
+    applySavedColors();
+  }
+};
+
+function classSelected(event) {
+  for (let k = 0; k < document.querySelectorAll('.color').length; k++) {
+    document.querySelectorAll('.color')[k].classList.remove('selected');
+    event.target.className = 'color selected';
+  }
 }
 
-function classSelected (event) {
-    for (let k = 0; k < document.querySelectorAll('.color').length; k ++) {
-        document.querySelectorAll('.color')[k].classList.remove('selected')
-        event.target.className = 'color selected'
-    }
+document
+  .querySelector('#color-palette')
+  .addEventListener('click', classSelected);
 
+function colorSelected(event) {
+  const toPaintColor =
+    document.querySelector('.selected').style.backgroundColor;
+  event.target.style.backgroundColor = toPaintColor;
+  storagePixels();
 }
-
-document.querySelector('#color-palette').addEventListener('click', classSelected);
-
-function colorSelected (event) {
-    const toPaintColor = document.querySelector('.selected').style.backgroundColor;
-    event.target.style.backgroundColor = toPaintColor
-    storagePixels();
-}
-
 
 function paintClick() {
-    const paintablePixel = document.querySelectorAll('.pixel');
-    console.log(paintablePixel);
-for (let p = 0; p < paintablePixel.length; p ++) {
- paintablePixel[p].addEventListener('click', colorSelected)
-}
+  const paintablePixel = document.querySelectorAll('.pixel');
+  console.log(paintablePixel);
+  for (let p = 0; p < paintablePixel.length; p++) {
+    paintablePixel[p].addEventListener('click', colorSelected);
+  }
 }
 
-const clearPixels = document.querySelector('#clear-board')
+const clearPixels = document.querySelector('#clear-board');
 clearPixels.addEventListener('click', () => {
-    const pixels = document.querySelectorAll('.pixel')
-    for (let c = 0; c < pixels.length; c ++){
-        pixels[c].style.backgroundColor = 'white'
-    }
-    })
+  const pixels = document.querySelectorAll('.pixel');
+  for (let c = 0; c < pixels.length; c++) {
+    pixels[c].style.backgroundColor = 'white';
+  }
+});
 
-function storagePixels () {
-    const paintedPixels = document.querySelectorAll('.pixel')
-    let savedColors = []
-    for (let s = 0; s < paintedPixels.length; s ++) {
-        savedColors.push(paintedPixels[s].style.backgroundColor)
-    }
-    localStorage.setItem('pixelBoard',JSON.stringify(savedColors))
+function storagePixels() {
+  const paintedPixels = document.querySelectorAll('.pixel');
+  let savedColors = [];
+  for (let s = 0; s < paintedPixels.length; s++) {
+    savedColors.push(paintedPixels[s].style.backgroundColor);
+  }
+  localStorage.setItem('pixelBoard', JSON.stringify(savedColors));
 }
 
-function applySavedColors () {
-    const applyColors = document.querySelectorAll('.pixel')
-    const savedColors = JSON.parse(localStorage.getItem('pixelBoard'))       
-    for (let a = 0; a < applyColors.length; a ++){
-        applyColors[a].style.backgroundColor = savedColors[a]
-        }
+function applySavedColors() {
+  const applyColors = document.querySelectorAll('.pixel');
+  const savedColors = JSON.parse(localStorage.getItem('pixelBoard'));
+  for (let a = 0; a < applyColors.length; a++) {
+    applyColors[a].style.backgroundColor = savedColors[a];
+  }
 }
 
 vqvButton.addEventListener('click', () => {
-    if (input.value === '') {
-        alert('Board inválido!')
-    }
-    else if (input.value < 5) {
-        input.value = 5
-    }
-    else if (input.value > 50) {
-        input.value = 50
-    }
-    createBoard(input.value);
-    localStorage.setItem('boardSize', input.value)
-})
+  if (input.value === '') {
+    alert('Board inválido!');
+  } else if (input.value < 5) {
+    input.value = 5;
+  } else if (input.value > 50) {
+    input.value = 50;
+  }
+  createBoard(input.value);
+  localStorage.setItem('boardSize', input.value);
+});
 
-function createBoard (px) {
-    tablePixels.innerHTML = ''
-    tablePixels.style.width = `${px * 40 }px`;
-    for (let index = 0; index < px*px; index ++) {
-        const pixel = document.createElement('div')
-        pixel.className = 'pixel'
-        tablePixels.appendChild(pixel);
-    }
-    tablePixels.style.gridTemplateColumns = `repeat(${px}, 1fr)`;
-
+function createBoard(px) {
+  tablePixels.innerHTML = '';
+  tablePixels.style.width = `${px * 40}px`;
+  for (let index = 0; index < px * px; index++) {
+    const pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    tablePixels.appendChild(pixel);
+  }
+  tablePixels.style.gridTemplateColumns = `repeat(${px}, 1fr)`;
 }
-
