7df632c877dd2c0c36ca2d317022eeecdd803d09
Arthur <acordeiro210691@gmail.com>
2022-11-04 23:51:45 -0300

    cria tablePixels e eventos de click para colorir

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const tablePixels = document.querySelector('#pixel-board');
+const input = document.querySelector('#board-size');
+const pixelsButton = document.querySelector('#generate-board');
+
 document.querySelectorAll('.color')
 function colorRandom (){
     let array = ['black']
@@ -28,3 +32,37 @@ if (colorPalette != null){
 function meuStorage (array){
     localStorage.setItem('colorPalette', JSON.stringify(array))
 }
+
+window.onload = () => {
+    tablePixels.style.width = `${5 * 40 }px`;
+    for (let index = 0; index < 25; index ++) {
+        const pixel = document.createElement('div')
+        pixel.className = 'pixel'
+        tablePixels.appendChild(pixel);
+    }
+    document.querySelectorAll('.color')[0].className = 'color selected'
+    paintClick();
+}
+
+function classSelected (event) {
+    for (let k = 0; k < document.querySelectorAll('.color').length; k ++) {
+        document.querySelectorAll('.color')[k].classList.remove('selected')
+        event.target.className = 'color selected'
+    }
+}
+
+document.querySelector('#color-palette').addEventListener('click', classSelected);
+
+function colorSelected (event) {
+    const toPaintColor = document.querySelector('.selected').style.backgroundColor;
+    event.target.style.backgroundColor = toPaintColor
+}
+
+function paintClick() {
+    const paintablePixel = document.querySelectorAll('.pixel');
+    console.log(paintablePixel);
+for (let p = 0; p < paintablePixel.length; p ++) {
+ paintablePixel[p].addEventListener('click', colorSelected)
+}
+}
+
